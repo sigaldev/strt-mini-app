b51cbd0c2e4febb016aff2fd2654660683a11228
Aigiz Tulybaev <flaylay256@gmail.com>
2025-11-12 16:30:16 -0500

    feat: add .env

--- src/components/api/api.ts
+++ src/components/api/api.ts
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 import AuthService from "./service/AuthService.ts";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true,
 });
 
